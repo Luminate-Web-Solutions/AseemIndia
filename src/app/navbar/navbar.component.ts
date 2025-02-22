a8e4c1a1bfb4b1ac09ec05f388aa9e8d00749ea7
Shahriar Hussain <shahriarhus@gmail.com>
2025-02-22 11:53:07 +0530

    update navbar links to use routerLink for improved routing; change navbar component to non-standalone

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule],
+  standalone: false,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
