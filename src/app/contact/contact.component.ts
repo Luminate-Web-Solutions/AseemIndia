6f0996993dc67ecd46d6fa862a1ebd158f3d6bd9
Iamfahad948 <fahadullah948@gmail.com>
2025-02-24 11:38:07 +0530

    Refactor contact and about components; update contact information, clean up HTML structure, and fix styleUrls reference

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-contact',
   standalone: false,
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
