176b7d108c086f7448183e695bb4d44b0114fecb
Iamfahad948 <fahadullah948@gmail.com>
2025-03-13 09:19:15 +0530

    Update page title, refactor navbar component, and enhance footer design with attribution

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -19,5 +18,28 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle nav', () => {
+    expect(component.isNavActive).toBeFalse();
+    component.toggleNav();
+    expect(component.isNavActive).toBeTrue();
+    component.toggleNav();
+    expect(component.isNavActive).toBeFalse();
+  });
+
+  it('should toggle dropdown', () => {
+    const event = new Event('click');
+    const target = document.createElement('a');
+    target.textContent = 'About Us';
+    spyOnProperty(event, 'target').and.returnValue(target);
+
+    component.toggleDropdown(event);
+    expect(component.isAboutDropdownActive).toBeTrue();
+    expect(component.isProjectsDropdownActive).toBeFalse();
+    expect(component.isContactDropdownActive).toBeFalse();
+
+    component.toggleDropdown(event);
+    expect(component.isAboutDropdownActive).toBeFalse();
   });
 });
